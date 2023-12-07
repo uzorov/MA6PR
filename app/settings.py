883b020476ef0384d0f4311ab1fcbc2477eb723c
Uzorov Kirill <uzorov_02@mail.ru>
2023-12-07 21:55:54 +0300

    Sixth practic are done

--- app/settings.py
+++ app/settings.py
@@ -2,9 +2,9 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    amqp_url: str = "amqp://guest:guest@localhost:5672"
+    amqp_url: str = "amqp://guest:guest@ma6pr-rabbitmq-1:5672"
     # postgres_url: str = "postgresql://:uzorov@maprac6-postgres-1:5432/test"
-    postgres_url: str = "postgresql://postgres:uzorov@localhost:5432/test"
+    postgres_url: str = "postgresql://postgres:uzorov@ma6pr-postgres-1:5432/test"
 
 
 settings = Settings()
